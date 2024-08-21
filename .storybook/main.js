54519d8a529e71821abf61b0f4e5eca9eb9c797b
Carmen Lopez <carmenbaez2000@gmail.com>
2024-08-21 18:46:53 +0200

    fix

---
+++ .storybook/main.js
@@ -0,0 +1,27 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@chromatic-com/storybook",
+      options: {
+        //👇 Loads the configuration file based on the current environment
+        configFile:
+          process.env.NODE_ENV === "development"
+            ? "chromatic.config.json"
+            : "production.config.json",
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;
