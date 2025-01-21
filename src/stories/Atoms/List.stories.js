00ec6400e3f1560f777eb8422393e88101b6c4d7
Carmen Lopez <carmenbaez2000@gmail.com>
2025-01-21 13:02:02 +0100

    TurboSnap test

--- src/stories/Atoms/List.stories.js
+++ src/stories/Atoms/List.stories.js
@@ -51,3 +51,11 @@ export const WithPlus = {
     icon: "+",
   },
 };
+
+export const WithLove = {
+  args: {
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    styleType: "custom-icon",
+    icon: "♡",
+  },
+};
