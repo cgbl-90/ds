7e47b126cc4c54e188cd885637ad1413708b2a17
Carmen Lopez <carmenbaez2000@gmail.com>
2025-02-26 16:22:06 +0100

    Tab Interaction test

--- src/stories/Atoms/Tab.stories.js
+++ src/stories/Atoms/Tab.stories.js
@@ -1,3 +1,4 @@
+import { within, userEvent, expect } from "@storybook/test";
 import { Tab } from "./Tab";
 
 export default {
@@ -18,4 +19,24 @@ Default.args = {
     { label: "Tab 2", content: "Content2" },
     { label: "Tab 3", content: "Content3" },
   ],
+};
+
+export const InteractionTab = Template.bind({});
+InteractionTab.args = { ...Default.args }; // Ensures it renders the UI
+
+InteractionTab.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const tab1 = canvas.getByRole("button", { name: /Tab 1/i });
+  const tab2 = canvas.getByRole("button", { name: /Tab 2/i });
+  const tab3 = canvas.getByRole("button", { name: /Tab 3/i });
+
+  await userEvent.click(tab1);
+  await expect(canvas.getByText("Content1")).toBeInTheDocument();
+
+  await userEvent.click(tab2);
+  await expect(canvas.getByText("Content2")).toBeInTheDocument();
+
+  await userEvent.click(tab3);
+  await expect(canvas.getByText("Content3")).toBeInTheDocument();
 };
