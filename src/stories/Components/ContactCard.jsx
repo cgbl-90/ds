6fb853ebfe06e18452179794bbfd7f44ee48f430
Carmen Lopez <carmenbaez2000@gmail.com>
2025-03-21 19:03:01 +0100

    fix

--- src/stories/Components/ContactCard.jsx
+++ src/stories/Components/ContactCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Icon } from "../Atoms/Icon";
+import { Avatar } from "../Atoms/Avatar";
 import { Typography } from "../Atoms/Typography";
 import Links from "../Atoms/Links";
 
 export const ContactCard = ({ iconSrc, name, email }) => (
   <div className="storybook-card">
-    <Icon size="medium" />
+    <Avatar src={iconSrc} size="small" />
     <Typography type="title" label={name} />
     <Links href={`mailto:${email}`} text={email} variant="primary" />
   </div>
